800fdc2ae6016956d0faaeefea989a129a1de4a3
HistoriaCruz User <historiacruz@example.com>
2026-07-19 11:38:44 -0500

    Corregir base para GitHub Pages

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/fotostudiodigitalmadueno4/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
